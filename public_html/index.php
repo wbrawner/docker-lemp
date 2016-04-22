7b556a55ddd2301ac9634fcb22292d5a4ff0f3cc
Billy Brawner <billybrawner@gmail.com>
2016-04-22 09:02:42 -0500

    Initial commit with basic LEMP stack

---
+++ public_html/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <h1>You're all set, get to coding!</h1>
+    </body>
+</html>
